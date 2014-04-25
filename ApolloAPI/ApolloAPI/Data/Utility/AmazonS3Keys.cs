8547c1fb6f38ea5418b7237be7705406429daa78
Bryan Lai <blty_2312@hotmail.my>
2014-04-25 22:50:15 +0800

    [FIX] Removal of existing Amazon keys

--- ApolloAPI/ApolloAPI/Data/Utility/AmazonS3Keys.cs
+++ ApolloAPI/ApolloAPI/Data/Utility/AmazonS3Keys.cs
@@ -10,12 +10,5 @@ namespace ApolloAPI.Data.Utility
         public string BucketName { get; set; }
         public string AccessKey { get; set; }
         public string SecretKey { get; set; }
-
-        public AmazonS3Keys()
-        {
-            BucketName = "apollo-ws";
-            AccessKey = "AKIAJBA4UZU67VMP3V3A";
-            SecretKey = "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";
-        }
     }
 }
